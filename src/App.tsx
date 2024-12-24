653b89e9ac7aa6bd745dedff7530d92aa62a61f1
coinvest518 <coinvest518@gmail.com>
2024-12-23 22:39:20 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
 import Referrals from './pages/Referrals';
-import PaymentCompletion from './pages/PaymentCompletions';
+import PaymentCompletion from './pages/PaymentCompletion';
 import Offers from './pages/Offers';
 import Payments from './pages/Payments';
 import Withdraw from './pages/Withdraw';
